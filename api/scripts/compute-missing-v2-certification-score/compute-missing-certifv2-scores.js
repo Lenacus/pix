7fc6fb7b1a311c42e50508c2448035580e5d5dd1
Thierry Loesch <tloesch@octo.com>
2019-07-23 10:09:09 +0200

    [pf-646] Get completed assessments only

--- api/scripts/compute-missing-v2-certification-score/compute-missing-certifv2-scores.js
+++ api/scripts/compute-missing-v2-certification-score/compute-missing-certifv2-scores.js
@@ -1,32 +1,21 @@
-/* eslint-disable no-console, no-unused-vars */
+'use strict';
+/* eslint-disable no-console */
 require('dotenv').config();
 const _ = require('lodash');
+const bluebird = require('bluebird');
 
 const { knex } = require('../../db/knex-database-connection');
 
 const createAssessmentResultForCompletedAssessment = require('../../lib/domain/usecases/create-assessment-result-for-completed-assessment');
 
-async function recomputeCertificationCoursesV2({
-  // Repositories
-  answerRepository,
-  assessmentRepository,
-  assessmentResultRepository,
-  certificationCourseRepository,
-  challengeRepository,
-  competenceRepository,
-  competenceMarkRepository,
-  courseRepository,
-  skillRepository,
-  // Services
-  scoringService
-}) {
+async function recomputeCertificationCoursesV2(dependencies) {
   let updatedCount = 0;
   const assessmentIds = await _getAssessmentIdsToRecompute();
-  for (const assessmentId of assessmentIds) {
+  await bluebird.map(assessmentIds, async (assessmentId) => {
     const progress = `(${++updatedCount}/${assessmentIds.length})`;
     try {
       await createAssessmentResultForCompletedAssessment({
-        ...arguments[0],
+        ...dependencies,
         ...{ assessmentId, forceRecomputeResult: true, updateCertificationCompletionDate: false }
       });
       console.log(`The assessment ${assessmentId} was successfully updated ${progress}`);
@@ -34,7 +23,7 @@ async function recomputeCertificationCoursesV2({
       console.log(`Unable to recompute assessment result for assessmentId : ${assessmentId} ${progress}`);
       console.error(err);
     }
-  }
+  }, { concurrency: (~~process.env.CONCURRENCY || 1) });
 }
 
 async function _getAssessmentIdsToRecompute() {
@@ -43,7 +32,8 @@ async function _getAssessmentIdsToRecompute() {
       .select('assessments.id as assessmentId')
       .from('assessments')
       .join('certification-courses', 'assessments.courseId', '=', knex.raw('cast("certification-courses".id as varchar)'))
-      .where('certification-courses.isV2Certification', true);
+      .where('certification-courses.isV2Certification', true)
+      .where('assessments.state', 'completed');
 
     return _.map(queryResults, 'assessmentId');
 
